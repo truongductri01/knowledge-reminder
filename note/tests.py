0cf9a7b3c1170c8f4925b6f470b9f615ae112492
Tri Truong <truongductri01@gmail.com>
2021-03-21 16:59:10 -0500

    reset database for note and add some tests

--- note/tests.py
+++ note/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import Note
+from api.models import User
 
 # Create your tests here.
+class NoteModelTests(TestCase):
+    user_name = "testing123"
+    password = "testing123"
+    user: User
+    def setUp(self) -> None:
+        User.objects.create(user_name=self.user_name, password=self.password)
+        self.user = User.objects.get(user_name=self.user_name)
+
+    def create_note(self, note_title):
+        Note.objects.create(note_title=note_title, user=self.user)
+        return Note.objects.get(note_title=note_title)
+
+    def test_note_set_is_empty_initially(self):
+        note_sets = self.user.note_set.all()
+        self.assertEqual(len(note_sets), 0)
+
+    def test_note_with_correct_title(self):
+        note_title = "this is a test note"
+        note = self.create_note(note_title)
+        self.assertEqual(note_title, note.note_title)
+
+    def test_add_one_note(self):
+        note_title = "this is a test note"
+        note = self.create_note(note_title)
+        note_sets = self.user.note_set.all()
+        self.assertEqual(len(note_sets), 1)
+        self.assertEqual(note, note_sets[0])
